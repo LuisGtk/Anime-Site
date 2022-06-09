b64f22768a3b6b5e94730c4a073239123b317c11
LuisGtk <luisgonzalez189@gmail.com>
2022-06-09 14:27:01 -0400

    working on useState

--- src/components/TopRated/TopRated.js
+++ src/components/TopRated/TopRated.js
@@ -1,18 +1,22 @@
 import './TopRated.css'
+// import {useState} from 'react' 
 export default function TopRated() {
-    const apiURL = "https://kitsu.io/api/edge/anime";
+    const apiURL = "https://kitsu.io/api/edge/trending/anime";
+    // const [rate, setRate] = useState(null)
 
-    function fetchBooks() {
+    function fetchAnime() {
         fetch(apiURL)
-            .then(res => res.json())
+            .then(Response => Response.json())
             .then(data => {
                 console.log(data);
             });
     }
 
+
     return (
         <div>
-
+            <button onClick={fetchAnime}> Fetch Anime</button>
+            <h2>{fetchAnime.canonicalTitle}</h2>
             <h2>Hello World</h2>
         </div>
 
