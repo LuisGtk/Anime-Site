2ea6bb01ab06307d37bd1d4258219b100c6b4781
LuisGtk <luisgonzalez189@gmail.com>
2022-06-09 14:06:42 -0400

    working on feching API data

--- src/components/TopRated/TopRated.js
+++ src/components/TopRated/TopRated.js
@@ -1,10 +1,19 @@
 import './TopRated.css'
 export default function TopRated() {
+    const apiURL = "https://kitsu.io/api/edge/anime";
+
+    function fetchBooks() {
+        fetch(apiURL)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            });
+    }
 
     return (
         <div>
-            <h2>Hello World</h2>
 
+            <h2>Hello World</h2>
         </div>
 
 
