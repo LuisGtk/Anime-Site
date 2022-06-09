1f5c6c8384690b0b21fc4f9efdaba0011e831cfb
LuisGtk <luisgonzalez189@gmail.com>
2022-06-09 13:17:51 -0400

    adding css to site/working on react-router-dom currently

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Genres from './components/Genres/Genres';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-<ul >
-  <li> Top-Rating </li>
-  <li> Genres </li>  
-  </ul>
-      </header>
-    </div>
+      <nav className="App-header">
+
+          <h1> Top-Rating </h1>
+          {/* <Link to={'/'}> */}
+            <h1> Genres </h1>
+          {/* </Link> */}
+
+      </nav>
+        <main>
+      {/* <Routes>
+        <Route path='/Genres' element={<Genres />} />
+      </Routes> */}
+    </main>
+    </div >
   );
 }
 
---
+++ src/components/Genres/Genres.js
@@ -0,0 +1,7 @@
+import './Genres.css'
+export default function Genres() {
+
+    return(
+        <h2>Hello World</h2>
+    )
+}
