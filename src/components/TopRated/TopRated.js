71e1ca0261e114e1c83220ec2dc704c5a3d2d4cd
LuisGtk <luisgonzalez189@gmail.com>
2022-06-16 13:57:26 -0400

    stuck on displaying API, able to dot into data in api but only one in an array at once

--- src/components/TopRated/TopRated.js
+++ src/components/TopRated/TopRated.js
@@ -5,24 +5,44 @@ import { useState } from 'react';
 export default function TopRated() {
     const apiURL = "https://kitsu.io/api/edge/trending/anime";
     const [anime, setAnime] = useState([])
-    // console.log(apiURL);
-    //////////////////////////////////////////////////////////////////////////////////
 
+    // console.log(apiURL);
+//////////////////////////////////////////////////////////////////////////////////
+
+// logs data
+    // function fetchAnime() {
+    //     fetch(apiURL)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             console.log(data);
+    //         });
+    // }
+
+// logs data array in data
     function fetchAnime() {
         fetch(apiURL)
-            .then(res => res.json())
+            .then(res => {
+                return res.json();
+            })
             .then(data => {
-                console.log(data);
-            });
+                console.log(data.data)
+                console.log(data.data[0].attributes.canonicalTitle )
+            })
     }
+    // fetchAnime()
 
-
-    ////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////9ij
 
     return (
         <div>
-            <h2>Here are the highly-rated animw at this time.</h2>
+            <h2>Here are the highly-rated anime at this</h2>
             <button onClick={fetchAnime}>Fetch Anime</button>
+            <ol>
+                {/* <li>  ({data.data[0].attributes.canonicalTitle})  </li> */}
+                <li>    </li>
+                <li>    </li>
+                <li>    </li>
+            </ol>
         </div>
     )
 }
