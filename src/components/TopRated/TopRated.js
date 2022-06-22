5f9703be03fad3b20895c3cdd1fc90db7445f720
LuisGtk <luisgonzalez189@gmail.com>
2022-06-22 14:15:35 -0400

    properly grabbed object data

--- src/components/TopRated/TopRated.js
+++ src/components/TopRated/TopRated.js
@@ -13,24 +13,22 @@ export default function TopRated() {
         //async function and useEffect to convert original api to proper json format // proper array
         const getData = async () => {
             const apiURL = "https://kitsu.io/api/edge/trending/anime"
-
-        try {
-            // fetching data
-            const res = await fetch
-        }
-        }
-    })
-
-
-
-
-
-
-
-
-
-
-
+            try {
+                // fetch data from link
+                const res = await fetch(apiURL);
+               // converting json response into a variable
+                const data = await res.json();
+                // console.log(data)
+                // dotting into array of anime inside the onbject
+                const objectData = Object.values(data)[0];
+                setAnime(objectData);
+                console.log(anime);
+            } catch (err) {
+                console.error(err)
+            }
+        };
+        getData();
+    }, [])
 
 
 
@@ -39,45 +37,36 @@ export default function TopRated() {
 
 
 
-    //logs data
-    // function fetchAnime() {
-    //     fetch(apiURL)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             console.log(data.data[0].attributes);
-    //         });
-    // }
-
-    // one way of console logging data
-
-    // function fetchAnime() {
-    //     fetch(apiURL)
-    //         .then(res => {
-    //             return res.json();
-    //         })
-    //         .then(data => {
-    //             console.log(data.data)
-    //             console.log(data.data[0].attributes.canonicalTitle )
-    //         })
-    // }
-    // fetchAnime()
-
-    ////////////////////////////////////////////////////////////////////////////////////
 
 
 
 
 
-    return (
-        <div>
-            <h2>Here are the highly-rated anime at this</h2>
-            {/* <button onClick={fetchAnime}>Fetch Anime</button> */}
-            <ol>
-                {/* <li>  ({data.data[0].attributes.canonicalTitle})  </li> */}
-                <li>    </li>
-                <li>    </li>
-                <li>    </li>
-            </ol>
-        </div>
-    )
+
+
+
+
+
+        //logs data
+        // function fetchAnime() {
+        //     fetch(apiURL)
+        //         .then(res => res.json())
+        //         .then(data => {
+        //             console.log(data.data[0].attributes);
+        //         });
+        // }
+
+        // one way of console logging data
+
+        // function fetchAnime() {
+        //     fetch(apiURL)
+        //         .then(res => {
+        //             return res.json();
+        //         })
+        //         .then(data => {
+        //             console.log(data.data)
+        //             console.log(data.data[0].attributes.canonicalTitle )
+        //         })
+        // }
+        // fetchAnime()
 }
