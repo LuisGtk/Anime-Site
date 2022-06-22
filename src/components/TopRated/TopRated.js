93dabaa70d0c2b9f8ed50be4aa957fd5673c8109
LuisGtk <luisgonzalez189@gmail.com>
2022-06-22 13:48:34 -0400

    working back on api

--- src/components/TopRated/TopRated.js
+++ src/components/TopRated/TopRated.js
@@ -1,42 +1,77 @@
+
 import './TopRated.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 ///////////////////////////////////////////////////////////////////////////
 
 export default function TopRated() {
-    const apiURL = "https://kitsu.io/api/edge/trending/anime";
     const [anime, setAnime] = useState([])
 
     // console.log(apiURL);
-//////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////
 
-// logs data
+    useEffect(() => {
+        //async function and useEffect to convert original api to proper json format // proper array
+        const getData = async () => {
+            const apiURL = "https://kitsu.io/api/edge/trending/anime"
+
+        try {
+            // fetching data
+            const res = await fetch
+        }
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //logs data
     // function fetchAnime() {
     //     fetch(apiURL)
     //         .then(res => res.json())
     //         .then(data => {
-    //             console.log(data);
+    //             console.log(data.data[0].attributes);
     //         });
     // }
 
-// logs data array in data
-    function fetchAnime() {
-        fetch(apiURL)
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                console.log(data.data)
-                console.log(data.data[0].attributes.canonicalTitle )
-            })
-    }
+    // one way of console logging data
+
+    // function fetchAnime() {
+    //     fetch(apiURL)
+    //         .then(res => {
+    //             return res.json();
+    //         })
+    //         .then(data => {
+    //             console.log(data.data)
+    //             console.log(data.data[0].attributes.canonicalTitle )
+    //         })
+    // }
     // fetchAnime()
 
-////////////////////////////////////////////////////////////////////////////////////9ij
+    ////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 
     return (
         <div>
             <h2>Here are the highly-rated anime at this</h2>
-            <button onClick={fetchAnime}>Fetch Anime</button>
+            {/* <button onClick={fetchAnime}>Fetch Anime</button> */}
             <ol>
                 {/* <li>  ({data.data[0].attributes.canonicalTitle})  </li> */}
                 <li>    </li>
