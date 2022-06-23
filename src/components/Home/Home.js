79a121d0593ac8bd2ac5f45387806ed58b63dddd
LuisGtk <luisgonzalez189@gmail.com>
2022-06-23 15:26:48 -0400

    adding search box

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,31 +1,34 @@
 import { useState, useEffect } from 'react';
 export default function Home() {
 
-        const [list, setList] = useState([]);
+    const [list, setList] = useState([]);
 
 
-        // useEffect(() => {
-        //     //async function and useEffect to convert original api to proper json format // proper array
-        //     const getData = async () => {
-        //         const apiURL = "https://kitsu.io/api/edge/anime"
-        //         try {
-        //             // fetch data from link
-        //             const res = await fetch(apiURL);
-        //             // converting json response into a variable
-        //             const data = await res.json();
-        //             console.log(data)
-        //             // dotting into array of anime inside the onbject
-        //             const objectData = Object.values(data)[0];
-        //             // adding object values  into objectData variable
-        //             setList(objectData);
-        //             console.log(objectData);
-        //         } catch (err) {
-        //             console.error(err)
-        //         }
-        //     };
-        //     getData();
-        // }, [])
-        return (
-            <input type="text" placeholder="Search.." name="search"></input>
-        )
-    }
+    // useEffect(() => {
+    //     //async function and useEffect to convert original api to proper json format // proper array
+    //     const getData = async () => {
+    //         const apiURL = "https://kitsu.io/api/edge/anime"
+    //         try {
+    //             // fetch data from link
+    //             const res = await fetch(apiURL);
+    //             // converting json response into a variable
+    //             const data = await res.json();
+    //             console.log(data)
+    //             // dotting into array of anime inside the onbject
+    //             const objectData = Object.values(data)[0];
+    //             // adding object values  into objectData variable
+    //             setList(objectData);
+    //             console.log(objectData);
+    //         } catch (err) {
+    //             console.error(err)
+    //         }
+    //     };
+    //     getData();
+    // }, [])
+    return (
+        <form className='charSearch'>
+            <input type="text" placeholder="Ex: Goku" name="search"></input>
+            <button type='submit'>Submit</button>
+        </form>
+    )
+}
